f88f69f02b7ffc16aa5370b8786020973c3aadf5
NECAU\Abhishek.Srivastava <abhishek.srivastava@nec.com.au>
2019-03-16 18:20:05 +1100

    initial commit

---
+++ src/test/java/io/javabrains/moviecatalogservice/TestController.java
@@ -0,0 +1,39 @@
+package io.javabrains.moviecatalogservice;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+public class TestController  extends MovieCatalogServiceApplication{
+	
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void testEmployee() throws Exception {
+		mockMvc.perform(get("/catalog/{userId}","foo")).andExpect(status().isOk());
+				//.andExpect(content().contentType("application/json;charset=UTF-8"));
+				//.andExpect(jsonPath("$.name").value("Lock, Stock and Two Smoking Barrels"));
+
+	}
+
+
+}
